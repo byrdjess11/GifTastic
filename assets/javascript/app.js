b3ab55bf19e99bedd2e35d8a0343a9a798d225aa
byrdjess <byrdjess@me.com>
2019-03-11 12:57:30 -0700

    jsjs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,37 +4,72 @@ function happyInfo() {
 
     var topic = $(this).attr("happy-data");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        happyStuff + "&api_key=T6UUxMapGH667fntbj2AmXuR24SEeQ91&limit=10";
+        topic + "&api_key=T6UUxMapGH667fntbj2AmXuR24SEeQ91&limit=10";
 
         $.ajax({
             url: queryURL,
             method: "GET"
-          })
+          }).then(function(results) {
 
-          .then(function(response) {
+
+            var results = response.data;
+
+            console.log(results)
             
-            var happyDiv = $("<div class='happy'>");
+            var happyDiv = $("<div>");
 
-            var rating = results[i].rating;
+            var rating = results.rating;
 
             var p = $("<p>").text("Rated: "  + rating);
 
             happyDiv.append(p);
 
-            var img = results[i].images.fixed_height.url;
+            var imgUrl = results.images.fixed_height.url;
 
-            var image = $("<img>").attr("src", img);
+            var image = $("<img>").attr("src", imgUrl);
 
-            happyDiv.append(image);
+            p.append(image);
 
             $("#gif-div").prepend(happyDiv);
+
       
           });
         
         }
 
-        
+function renderButtons() {
 
+  $("#buttons-div").empty();
+
+  for (var i = 0; i < topics.length; i++) {
+
+    var btn = $("<button>");
+
+    btn.addClass("happy-btn");
+    btn.attr("happy-data", topics[i]);
+    btn.text(topics[i]);
+
+    $("#buttons-div").append(btn);
+
+
+    }
+  }
+
+  $("#add-happy").on("click", function(event) {
+    event.preventDefault();
+
+    var topic = $("#happy-input").val().trim();
+
+    topics.push(topic);
+
+    renderButtons();
+
+  });
+
+
+  $(document).on("click", ".happy-btn", happyInfo);
+
+  renderButtons();
 
 
 
